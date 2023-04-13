a6b1c4ad091b0b411cdbd62c514513a10c7f031d
rjalabado <rjalabado@gmail.com>
2023-04-13 14:22:59 -0700

    Initial Commit

---
+++ Test App xCode/Test_App_xCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_App_xCodeApp.swift
+//  Test App xCode
+//
+//  Created by RRAlabado on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_App_xCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
